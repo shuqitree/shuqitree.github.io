22d66c5c433eeb2d12db9a4828e24001f6b7dd76
Shuqi Jiang <949021208@qq.com>
2023-08-26 22:41:40 -0400

    Update the logic of automatic script

--- json2site.js
+++ json2site.js
@@ -20,7 +20,7 @@ const pandoc_markdown = async md => {
 }
 
 const PAGEGEN =
-	[[IS_FILM, (html, { title, yt, md, stills }) => {
+	[[IS_FILM, (html, { title, yt, md, date, stills }) => {
 		const yt_disp = yt
 			? `<iframe class=film-yt src="https://www.youtube.com/embed/${yt}" title="YouTube player for ${title}" frameborder=0 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
 			: ''
@@ -29,7 +29,7 @@ const PAGEGEN =
 			: `<div class=stills>${stills.map(src => `<img class=still src='${src}'>`).join('')}</div>`
 
 		return `<div class=film-intro><h2 class=film-title>${title}</h2><div class=film-medium>${md}</div>${html}</div>`
-			+ yt_disp + stills_disp
+			+ yt_disp + `<span class=story-date>${date}</span>` + stills_disp 
 	}]
 		, [IS_PHOTOGRAPHY, (html, { title, stills }) => {
 			const stills_disp = stills.length === 0
